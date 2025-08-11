f10daf09c63e2c95bfd50735e3b92f45c59daa17
Diovi Robinet Morales <drobinetm@outlook.com>
2025-08-11 19:34:27 -0400

    fix: Add docker in the project.
    Fix the Dockerfile config in the projects.
    
    BUNDLE: Fixing the Dockerfile and build the docker compose

--- apps/backend-django/app/urls.py
+++ apps/backend-django/app/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("service_app.urls")),
     path("admin/", admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path("api/", include('rest_framework.urls')),
 ]

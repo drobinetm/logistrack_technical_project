c8b595f89e73bccc50817438973f5c6a0067544c
Diovi Robinet Morales <drobinetm@outlook.com>
2025-08-12 08:12:51 -0400

    fix: Move the structure project to modules folder
    BUNDLE_STRUCTURE: Moving to new folder

---
+++ modules/backend-django/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+echo "Waiting MySQL..."
+until nc -z mysql_django 3306; do
+  sleep 1
+done
+echo "MySQL Done ✅"
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Init Django with Gunicorn..."
+exec gunicorn app.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3
---
+++ modules/backend-symfony/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+echo "Waiting MySQL..."
+until nc -z mysql_symfony 3306; do
+  sleep 1
+done
+echo "MySQL está listo."
+
+echo "Installing dependencies with Composer..."
+composer install --no-interaction --prefer-dist --no-scripts
+
+# Skip migrations for now due to doctrine/orm issues
+echo "Running migrations..."
+php bin/console doctrine:migrations:migrate --no-interaction
+
+echo "Starting Apache..."
+exec apache2-foreground
